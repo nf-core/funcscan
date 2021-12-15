9d3185f9673549bae8074a162e0b98655fd7715e
James A. Fellows Yates <jfy133@gmail.com>
2021-12-15 11:30:27 +0100

    Apply suggestions from code review
    Co-authored-by: Jasmin F <73216762+jasmezz@users.noreply.github.com>

--- lib/WorkflowFuncscan.groovy
+++ lib/WorkflowFuncscan.groovy
@@ -13,7 +13,7 @@ class WorkflowFuncscan {
         //if (!params.fasta) {
         //    log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
         //    System.exit(1)
-        }
+        //}
     }
 
     //
