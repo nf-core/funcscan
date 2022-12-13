f23273634b47c5d051d31af5fed32956b58bf3cd
jasmezz <jasmin.frangenberg@hki-jena.de>
2022-12-13 17:21:34 +0100

    Update comBGC script draft

--- modules/local/comBGC.py
+++ modules/local/comBGC.py
@@ -52,10 +52,9 @@ def extract_gbk_info(as_dirs, contigs):
 					elif feature.type == "CDS" and "translation" in feature.qualifiers.keys() and feature.location.end <= BGC_end: # Make sure not to count pseudogenes (which would have no "translation tag") and within the current BGC region
 						CDS_count += 1
 
-					infile = '{}knownclusterblast/{}_c1.txt'.format(as_dir, record.id)
-					clust_IDs, clust_annotations, blast_num, blast_identity_averages, blast_score_cum, blast_score_averages, blast_coverage_averages, blast_cds_annotations = parse_cluster(infile)
+					kcb_file = '{}knownclusterblast/{}_c1.txt'.format(as_dir, record.id) # Prbly need dir list to get file names, bc ..._c2.txt can also occur!
+					MIBiG_IDs = parse_knownclusterblast(kcb_file)
 					BGC_pos = record.annotations["structured_comment"]
-					Contig_edge, error_id = edge_position(contigs, antismash_output_folder, record.id, BGC_pos) # Get the side of the contig edge (left/right/both/none) #### Buth this into as_results and TSV output
 
 					# Store the all the values for current GBK in a list
 					as_results[Sample_ID + gbk] = [Sample_ID,
@@ -74,100 +73,23 @@ def extract_gbk_info(as_dirs, contigs):
 										blast_cds_annotations]
 	return as_results
 
-# Function: Determine on which side of the contig the BGC stops. Region of 50 base pairs on each end counts as truncated
-def edge_position(contigs, sample, id, bgc_pos):
-	bgc_start = int(bgc_pos["antiSMASH-Data"]["Orig. start"]) # BGC start position in contig
-	bgc_end = int(bgc_pos["antiSMASH-Data"]["Orig. end"]) # BGC stop position in contig
+# Function: Read in the antiSMASH txt file and extract MIBiG IDs
+def parse_knownclusterblast(kcb_file):
+    with open(kcb_file) as infile:
+        hits = 0
+        MIBiG_IDs = []
 
-	try:
-		if bgc_start <= 50 and bgc_end >= contigs[sample][id] - 50: # Contig edge on both sides
-			side = "left, right"
-		elif bgc_start > 50 and bgc_end < contigs[sample][id] - 50: # No contig edge
-			side = "no"
-		elif bgc_start <= 50 and bgc_end < contigs[sample][id] - 50: # Contig edge on the left
-			side = "left"
-		elif bgc_start > 50 and bgc_end >= contigs[sample][id] - 50: # Contig edge on the right
-			side = "right"
-		error = ""
-	except KeyError: # Return contig IDs for which no entry in the prokka file was found
-		side = "NA"
-		error = id
+        for line in infile:
+            if line == "Significant hits: \n" and not hits:
+                hits = 1
+            elif line == "\n" and hits:
+                break
 
-	return side, error
+            if hits:
+                MIBiG_ID = re.match("(BGC\d+)", line).group(1)
+                MIBiG_IDs.append(MIBiG_ID)
 
-# Function: Read in the antiSMASH txt file and split it into the records (divided by ">>")
-def file_to_list(file_path):
-	hits = open(file_path).read()
-
-	# Divide the hits into a list of lists:
-	# First list level: All the records divided by ">>"
-		# Second list level: Metadata at the top of each record, genes with annotations, significant BLAST hits
-		# Final list looks like this: [[Metadata1, genes with annotation1, BLAST hits1], [Metadata2, genes with annotation2, BLAST hits2], [...]]
-	hits = hits.split("\n\n>>\n")[1:] # Split by record
-	for i in range(len(hits)): # Split each record into metadata, annotations and significant BLAST hits
-		hits[i] = hits[i].split("\nTable of genes, locations, strands and annotations of subject cluster:\n")
-		metadata = hits[i][0].rstrip().split("\n")
-
-		annotation_data = hits[i][1].split("\nTable of Blast hits (query gene, subject gene, %identity, blast score, %coverage, e-value):\n")
-		annotations = annotation_data[0].rstrip().split("\n")
-		annotations = [gene.split("\t") for gene in annotations]
-
-		blast_hits = annotation_data[1].rstrip().split("\n")
-		blast_hits = [hit.split("\t") for hit in blast_hits]
-		hits[i] = [metadata, annotations, blast_hits]
-	return[hits]
-
-# Function: Parse the antiSMASH KnownclusterBLAST txt files (MIBiG IDs)
-def parse_cluster(file_path): # Function for calculating the average BLAST hits identity value and extracting datails from the txt files
-
-	# Get a list of all the records in the antiSMASH txt file
-	records = file_to_list(file_path)
-
-	# Extract and calculate the results from that list
-	## (Known)clusterblast ID
-	clust_IDs = [re.search("\d+\. (.*)", metaline[0][0]).group(1) for metaline in records[0]]
-
-	## (Known)clusterblast annotations
-	clust_annotations = [re.search("Source: (.*)", metaline[0][1]).group(1) for metaline in records[0]]
-	for j in range(len(clust_annotations)):  # Make annotations uppercase
-		clust_annotations[j] = clust_annotations[j][0].upper() + clust_annotations[j][1:]
-
-	## Cumulative BLAST score
-	blast_score_cum = [re.search("Cumulative BLAST score: (\d+)", metaline[0][4]).group(1) for metaline in records[0]]
-
-	## Average BLAST identity to (Known)clusterblast genes
-	blast_num = []
-	blast_identity_averages = []
-	blast_score_averages = []
-	blast_coverage_averages = []
-	blast_cds_annotations = []
-
-	for record in records[0]:
-
-		# Store (Known)clusterblast IDs and corresponding annotations in dictionary (if present)
-		try:
-			gene_match_ids = {line[0]: line[5] for line in record[1]} # Dictionary of (Known)clusterblast gene IDs and annotations
-		except IndexError:
-			gene_match_ids = {}
-
-		# Extract annotation IDs, identity values, BLAST scores and subject coverage from BLAST hits table
-		blast_hit_ids = [line[1] for line in record[2]]
-		blast_hit_identities = [float(line[2]) for line in record[2]]
-		blast_hit_scores = [float(line[3]) for line in record[2]]
-		blast_hit_coverages = [float(line[4]) for line in record[2]]
-
-		# Get number of BLAST hits per record
-		blast_num.append(str(len(record[2])))
-
-		# Get (Known)clusterblast cds_annotations
-		blast_cds_annotations.append([gene_match_ids[hit_id] for hit_id in blast_hit_ids if hit_id in gene_match_ids.keys()])
-
-		# Calculate avarage identity score
-		blast_identity_averages.append(str(round(sum(blast_hit_identities)/len(blast_hit_identities), 2)))
-		blast_score_averages.append(str(round(sum(blast_hit_scores)/len(blast_hit_scores), 2)))
-		blast_coverage_averages.append(str(round(sum(blast_hit_coverages)/len(blast_hit_coverages), 2)))
-
-	return clust_IDs, clust_annotations, blast_num, blast_identity_averages, blast_score_cum, blast_score_averages, blast_coverage_averages, blast_cds_annotations
+    return MIBiG_IDs
 
 # Dictionary with all the results to be written to disk
 print("\nParsing these samples:")
