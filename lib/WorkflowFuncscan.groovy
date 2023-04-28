9b0989eec4e32a2892a254c548e2ff1a54eaec37
Jasmin Frangenberg <73216762+jasmezz@users.noreply.github.com>
2023-04-28 20:45:26 +0000

    Fix error in resolving merge conflict

--- lib/WorkflowFuncscan.groovy
+++ lib/WorkflowFuncscan.groovy
@@ -13,9 +13,9 @@ class WorkflowFuncscan {
     public static void initialise(params, log) {
         genomeExistsError(params, log)
 
-        if (!params.fasta) {
-            Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-        }
+        //if (!params.fasta) {
+        //    Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+        //}
     }
 
     //
